0017ae37c4d5323e840743653a04c0978376ec24
Joyce Achieng <joyce.achieng@student.moringaschool.com>
2023-12-09 11:42:58 +0300

    Complete Assignment

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES
+(NULL, 5, 'F', 'Brown', 'Calm', 1),
+('Mr. Chocolate', 5, 'M', 'Brown', 'Calm', 1),
+('Rowdy', 3, 'M', 'Black', 'Energetic', 1),
+('Tabitha', 7, 'F', 'White', 'Playful', 1),
+('Sergeant Brown', 8, 'M', 'Brown', 'Brave', 1),
+('Melissa', 4, 'F', 'Gray', 'Friendly', 1),
+('Grinch', 6, 'M', 'Green', 'Grumpy', 1),
+('Wendy', 2, 'F', 'Brown', 'Shy', 1);
+
